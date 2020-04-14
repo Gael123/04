4cb967bc1de8547d0ba69bb825ef15e901c993e4
Gael Jerop <gael.jerop@yahoo.com>
2020-04-14 15:10:51 +1000

    created repo

--- theme.js
+++ theme.js
@@ -1,7 +1,21 @@
 export const theme = {
-  // Customize your presentation theme here.
-  //
-  // Read the docs for more info:
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+    fonts: {
+        body: 'Roboto, sans-serif',
+        monospace: '"Roboto Mono" , monospace',
+
+    },
+    colors: {
+
+        h2: '#fff',
+        text: 'black',
+        background: 'orange',
+        primary: 'blue',
+
+    },
+
+    // Customize your presentation theme here.
+    //
+    // Read the docs for more info:
+    // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+    // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
 }
